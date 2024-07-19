2bc9b2950117d03a90b9b87aee4047b51285a0a2
lbmunaro <lucasb4@illinois.edu>
2024-07-18 21:31:52 -0500

    Predict MT-GBLUP

--- Step6_GEBVs_MT-GBLUP_fa3.R
+++ Step6_GEBVs_MT-GBLUP_fa3.R
@@ -12,10 +12,10 @@ library(asreml) # ASReml-R package
 
 # Load data ----
 ## BLUES & Gmat ----
-load('Data/Step5_MT-GBLUP.RData')
+load('Data/Step5_MT-GBLUP_fa3.RData')
 
 # Predict GEBVs ----
 GEBVs_MT <- predict(mod0_MT.GBLUP, classify='germplasm:group', pworkspace='16gb')$pvals
 
 # Save ----
-save.image('Data/Step6_GEBVs_MT-GBLUP.RData')
+save.image('Data/Step6_GEBVs_MT-GBLUP_fa3.RData')
